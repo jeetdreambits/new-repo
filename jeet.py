4956e3edef93e983177bfc9b56a2af2a464b68af
jeetpatel0908 <parthpatel01911@gmail.com>
2026-03-05 14:36:51 +0530

    test

--- jeet.py
+++ jeet.py
@@ -5,14 +5,6 @@ class GithubEvent(models.Model):
     
     event_type = fields.Char("Event Type")
     repository = fields.Char("Repository")
-rrrrff
-
-    
     author = fields.Char("Author") 
-
-    
     message = fields.Text("Message")    
     created_at = fields.Datetime("Created At")
-
-
-
