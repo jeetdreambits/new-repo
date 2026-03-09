8ae534ac84af04b765e992fec9f5a6e5cc93288f
jeetpatel0908 <parthpatel01911@gmail.com>
2026-03-09 10:26:21 +0530

    Add blank line for code formatting in jeet.py

--- jeet.py
+++ jeet.py
@@ -2,13 +2,14 @@ class GithubEvent(models.Model):
     _name = "github.event"
     _description = "GitHub Event"
     
+
+
+
     
     event_type = fields.Char("Event Type")
     repository = fields.Char("Repository")
     author = fields.Char("Author") 
     message = fields.Text("Message")    
-
-
 
     
     created_at = fields.Datetime("Created At")
