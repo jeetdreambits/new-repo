cb8e220ab1b49f5fc510d0e9602deb0b54f39a3c
jeetpatel0908 <parthpatel01911@gmail.com>
2026-03-20 10:57:32 +0530

    Clean up blank lines in GithubEvent model
    Removed unnecessary blank lines in GithubEvent class

--- jeet.py
+++ jeet.py
@@ -2,10 +2,6 @@ class GithubEvent(models.Model):
     _name = "github.event"
     _description = "GitHub Event"
 
-
-
-    
-
     event_type = fields.Char("Event Type")
     repository = fields.Char("Repository")
     author = fields.Char("Author") 
